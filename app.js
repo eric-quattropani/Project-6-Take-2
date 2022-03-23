4414c760b37fa556a90407504de8baa3fbfd2c44
eric-quattropani <eric.quattropani@gmail.com>
2022-03-24 08:39:47 +1100

    Initial commit
    Starting over again - copying over code done previously

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const { projects } = require('./data.json');
 const app = express();
+
+app.use(express.json());
 
 app.set('view engine', 'pug');
 
@@ -8,8 +11,29 @@ const about = require('./routes/about');
 
 app.use('/static', express.static('public'));
 
-app.use('/', index);
-app.use('/about', route);
+//app.use('/', index);
+// app.use('/about', route);
+
+app.get('/', (req, res, next) => {
+  res.render('index', { projects });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+/* Dynamic "project" routes */
+app.get('/projects/:id', (req, res, next) => {
+  const id = req.params.id;
+  const project = projects[id];
+      if (project) {
+          res.render('project', { project });
+  } else {
+          const err = new Error;
+          err.status = 404
+          err.message = `Project ${id} does not exist`;
+          next(err);
+      }
+});
 
 app.use((req, res, next) => {
   console.log('Oops!  It looks like something went wrong on the server.')
@@ -30,3 +54,6 @@ app.use((err, req, res, next) => {
 app.listen(3000, () => {
   console.log('listen on port 3000');
 });
+
+
+// look at this: https://github.com/pattherogue/Static-Node.js-and-Express-Site/blob/main/data.json
