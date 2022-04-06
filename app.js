be2d1858a6868e4a9dc13e3edc68d429c0637c21
eric-quattropani <eric.quattropani@gmail.com>
2022-04-07 09:26:24 +1000

    Update app.js

--- app.js
+++ app.js
@@ -31,19 +31,8 @@ app.get('/projects/:id', (req, res, next) => {
 });
 
 app.use((req, res, next) => {
-  console.log('Oops!  It looks like something went wrong on the server.')
+  console.log('Oops! Something went wrong...')
   res.status(404).render('page-not-found');
-});
-
-app.use((err, req, res, next) => {
-
-  if (err.status === 404) {
-    console.log('Something went wrong')
-    res.status(404).render('page-not-found', { err });
-  } else {
-    err.message = err.message || 'Something went wrong';
-    res.status(err.status || 500).render('error', { err });
-  };
 });
 
 app.listen(3000, () => {
